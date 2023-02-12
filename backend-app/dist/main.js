9af265f5a47e99f95895ef21f7fd1d0057395d2b
Aung Kyaw Nyunt <aungkyawnyunt2004@gmail.com>
2023-02-12 20:16:03 +0700

    [create] init backend project

---
+++ backend-app/dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const configuration_1 = require("./config/configuration");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen((0, configuration_1.default)().port);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
